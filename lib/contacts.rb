5812fdca0a13da441cad636aeb74de638dc79f36
Beverly Appleton <bfcain19@gmail.com>
2020-04-03 08:10:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,5 +16,9 @@
 
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    conact_details_hash.each do |attribute, data|
+    end
+  end
 
 end
